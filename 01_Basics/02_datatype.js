8048e061b0859292cd4af23cc2ed14d0904d4bd7
Mohd Salman <111675665+Salman-1432@users.noreply.github.com>
2024-09-22 14:53:41 +0000

    datatype

---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,12 @@
+"use strict";// treat all js code as newer version
+//alert(3+3)// we are using nodeJs ,not browser
+//console.log("slana"); console.log("usman");
+//number=>2 to power 53
+//bigint
+//string=""
+//boolean=>true/false
+//null=>standalone vlaue
+//undefined=>
+//symbol=>Unique
+//object
+console.log(typeof Object);
